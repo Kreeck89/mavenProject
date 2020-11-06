ee26e0dcb8a614b7b73787280a058065b69adf94
kreeck89 <kreeckkreeck@gmail.com>
2020-11-07 00:52:21 +0200

    Example with lombok library

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import entities.User;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setName("sdsds");
+        System.out.println(user.getName());
+
+    }
+}
---
+++ src/main/java/entities/User.java
@@ -0,0 +1,12 @@
+package entities;
+
+
+import lombok.*;
+
+import java.util.Objects;
+
+@Data
+public class User {
+    private String name;
+
+}
